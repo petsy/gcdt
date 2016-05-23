e1e93bb6b65e44a0f5718bba1cadbc6b2c0f352f
Goetz Epperlein <goetz.epperlein@gmx.de>
2016-05-23 16:00:36 +0200

    FIX merge latest dp version adding lookup stack param

--- gcdt/kumo_util.py
+++ gcdt/kumo_util.py
@@ -36,50 +36,61 @@ class StackLookup(object):
     Class to handle stack lookups
     """
 
-    PARAMETER_LOOKUP_PREFIX = 'CfOut_'
-
-    def __init__(self, template, param_lambda_lookup_arn, param_stack_dependent_on):
+    def __init__(self, template, param_lambda_lookup_arn, param_stack_dependent_on=None):
         """
         Adds function to cloudformation template to lookup stack information
         :param template: The cloudformation template
         :param param_lambda_lookup_arn: The parameter stating the ARN of the COPS provided Lambda lookup function
-        :param param_stack_dependent_on: The parameter stating the stack name which should be lookedup from
+        :param param_stack_dependent_on: The parameter stating the stack name which should be lookedup from (Default: None)
         """
+        if param_stack_dependent_on is not None:
+            class CustomStackOutput(AWSCustomObject):
+                resource_type = "Custom::StackOutput"
 
-        class CustomStackOutput(AWSCustomObject):
-            resource_type = "Custom::StackOutput"
+                props = {
+                    'ServiceToken': (basestring, True),
+                    'StackName': (basestring, True)
+                }
 
-            props = {
-                'ServiceToken': (basestring, True),
-                'StackName': (basestring, True)
-            }
+            self.__custom_stack_obj = template.add_resource(CustomStackOutput(
+                "StackOutput",
+                ServiceToken=troposphere.Ref(
+                    param_lambda_lookup_arn
+                ),
+                StackName=troposphere.Ref(param_stack_dependent_on),
+            ))
+        else:
+            class CustomStackOutput(AWSCustomObject):
+                resource_type = "Custom::StackOutput"
 
-        self.__custom_stack_obj = template.add_resource(CustomStackOutput(
-            "StackOutput",
-            ServiceToken=troposphere.Ref(
-                param_lambda_lookup_arn
-            ),
-            StackName=troposphere.Ref(param_stack_dependent_on),
-        ))
+                props = {
+                    'ServiceToken': (basestring, True)
+                }
 
-    def get_att(self, parameter):
+            self.__custom_stack_obj = template.add_resource(CustomStackOutput(
+                "StackOutput",
+                ServiceToken=troposphere.Ref(
+                    param_lambda_lookup_arn
+                ),
+            ))
+
+    def get_att(self, parameter, as_reference=True):
         """
         Retrieves an attribute from an existing stack
         :param parameter: The output parameter which should be retrieved
+        :param as_reference: Is the parameter a reference (Default) or a string
         :return: Value of parameter to retrieve
         """
-
-        # TODO fix concat prefix -> see support ticket
-        return troposphere.GetAtt(
-            self.__custom_stack_obj,
-            # troposphere.Join(
-            #     "",
-            #     [
-            #         self.PARAMETER_LOOKUP_PREFIX,
-            troposphere.Ref(parameter)
-            #     ]
-            # )
-        )
+        if as_reference:
+            return troposphere.GetAtt(
+                self.__custom_stack_obj,
+                troposphere.Ref(parameter)
+            )
+        else:
+            return troposphere.GetAtt(
+                self.__custom_stack_obj,
+                parameter
+            )
 
 
 def json2table(json):
@@ -129,6 +140,7 @@ def get_stack_id(stackName):
     stack_id = response["Stacks"][0]["StackId"]
     return stack_id
 
+
 def create_dp_name(env, layer, name):
     if env == "dev":
         return "dp-dev-" + layer + "-" + name
@@ -136,6 +148,7 @@ def create_dp_name(env, layer, name):
         return "dp-prod" + layer + "-" + name
     else:
         raise Exception("Unknown env: " + env)
+
 
 def poll_stack_events(stackName):
     finished_statuses = ["CREATE_COMPLETE",
