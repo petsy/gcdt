ec70c992e131ea6df01f0324d21cfbc8eaf0db7b
Johannes Brandstetter <johannes@jb.gl>
2016-05-30 17:28:27 +0200

    changes according to pull request comments

--- tests/test_config_reader.py
+++ tests/test_config_reader.py
@@ -1,26 +1,51 @@
 from unittest import TestCase, main
 import sys
 from pyhocon import ConfigFactory
+from gcdt.logger import setup_logger
+from mock import Mock, MagicMock, patch
 
-sys.path.append("../gcdt/")
+# Same response for all lockups
+cloudformation_response = {
+  "ClusterEndpointAddress": "redshift-test.dp.glomex.cloud",
+  "ClusterEndpointPort": "5000",
+  "RoleLambda": "arnApi",
+  "SecurityGroup": "sg-ae6850ca"
+}
 
-from config_reader import resolve_lookups
+class ConfigReaderTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """
+        Prepare environment
+        """
+        # Load files to test
+        sys.path.insert(1, '../dphelper')
+        # Import file under test
+        import config_reader
+        # Make it accessible outside this method
+        global config_reader
 
-class MonitoringTestCase(TestCase):
-    def setUp(self):
-        pass
+        # Build test logger
+        log = setup_logger(logger_name="dphelper_configreader_test")
 
-    def tearDown(self):
-        pass
+    def test_read(self):
+        conf = config_reader.read_config("sample_settings", "resources", do_lookups=False)
+        self.assertEquals(conf.get("redshift.stackName"), "dp-dev-store-redshift")
 
-    def test_resolve_lookups(self):
-        config_string = """lambda {
-            applicationName = "lookup:dp-dev-operations-jenkins-jobr:ApplicationName",
-            sslcert = "ssl:wildcard.glomex.com"
-                            }"""
-        conf = ConfigFactory.parse_string(config_string)
-        print resolve_lookups(conf)
+    @patch('config_reader.servicediscovery.get_outputs_for_stack',return_value=cloudformation_response)
+    def test_lookup(self, get_outputs_for_stack_function):
 
+        conf = config_reader.read_config("sample_settings", "resources", do_lookups=True)
+
+        self.assertEquals(conf.get("redshift.clusterEndpointPort"), "5000")           # Lookup
+
+        self.assertEquals(conf.get("lambdas.entries")[0].get("role"), "arnApi")       # Lookup in complex array
+
+        self.assertEquals(conf.get("lambdas.vpc.subnetIds")[0], "subnet-87685dde")    # Keep in array
+
+        self.assertEquals(conf.get("lambdas.vpc.securityGroups")[0], "sg-ae6850ca")   # Lookup in array
+
+        self.assertEquals(conf.get("lambdas.timeout"), 300)                           # Keep int
 
 if __name__ == "__main__":
     main()
