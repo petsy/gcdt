d463e5d766ac87cbc62785bde6ad58d7021d8687
Johannes Brandstetter <johannes@jb.gl>
2016-06-20 11:28:34 +0200

    Bump version: 0.0.12.dev0 → 0.0.12.dev1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = '0.0.12.dev0'
+__version__ = '0.0.12.dev1'
 
 here = path.abspath(path.dirname(__file__))
 
