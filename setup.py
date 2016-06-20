8dc9cb50337d0c4209a52cce39819c6e6b25451a
Jenkins <jenkins@dp.glomex.cloud>
2016-06-20 15:14:21 +0200

    Bump version: 0.0.12rc → 0.0.13rc

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = '0.0.12rc'
+__version__ = '0.0.13rc'
 
 here = path.abspath(path.dirname(__file__))
 
