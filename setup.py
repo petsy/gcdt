53a570431dfc076b6db725daac48752ae2cedf79
Johannes Brandstetter <johannes@jb.gl>
2016-06-20 14:18:16 +0200

    Bump version: 0.0.11rc → 0.0.12rc

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = '0.0.11rc'
+__version__ = '0.0.12rc'
 
 here = path.abspath(path.dirname(__file__))
 
