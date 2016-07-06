d1f7dfe217d6f970ce140c6aa6c7c035d91d314b
Andreas Sieferlinger <andreas.sieferlinger@prosiebensat1digital.de>
2016-07-06 08:22:14 +0200

    switching from zip file (due to max file count restrictions) to tar.gz

--- gcdt/tenkai_utils.py
+++ gcdt/tenkai_utils.py
@@ -1,22 +1,23 @@
 import io
 import os
 from zipfile import ZipFile
+import tarfile
 import sys
 import threading
 
 
-def files_to_zip(path):
+def files_to_bundle(path):
     for root, dirs, files in os.walk(path):
         for f in files:
             full_path = os.path.join(root, f)
             archive_name = full_path[len(path) + len(os.sep):]
-            # print "full_path, archive_name" + full_path, archive_name
+            #print "full_path, archive_name" + full_path, archive_name
             yield full_path, archive_name
 
 def make_zip_file_bytes(path):
     buf = io.BytesIO()
     with ZipFile(buf, 'w') as z:
-        for full_path, archive_name in files_to_zip(path=path):
+        for full_path, archive_name in files_to_bundle(path=path):
             # print "full_path " + full_path
             archive_target = "/" + archive_name
             # print "archive target " + archive_target
@@ -24,6 +25,16 @@ def make_zip_file_bytes(path):
     #buffer_mbytes = float(len(buf.getvalue()) / 10000000)
     #print "buffer has size " + str(buffer_mbytes) + " mb"
     return buf.getvalue()
+
+def make_tar_file(path, outputpath):
+    # make sure we add a unique identifiere when we are running within jenkins
+    file_suffix = os.getenv('BUILD_TAG', '')
+    destfile = '%s/tenkai-bundle%s.tar.gz' % (outputpath, file_suffix)
+    with tarfile.open(destfile, "w:gz") as tar:
+        for full_path, archive_name in files_to_bundle(path=path):
+            tar.add(full_path, recursive=False, arcname=archive_name)
+    return destfile
+
 
 
 class ProgressPercentage(object):
