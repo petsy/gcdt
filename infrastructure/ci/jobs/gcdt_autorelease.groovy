10377af92f6f1423e6b0270216e6e123a3d4790d
mark <mark@mark-fink.de>
2016-10-26 17:08:54 +0200

    added build dputilities dependency

--- infrastructure/ci/jobs/gcdt_autorelease.groovy
+++ infrastructure/ci/jobs/gcdt_autorelease.groovy
@@ -5,8 +5,8 @@ def branchToCheckout = DpUtilities.getBranch()
 out.println(branchToCheckout)
 
 def credentialsToCheckout = "psd-frontend-jenkins_username-password"
-def configFile = readFileFromWorkspace("./operations/continous-delivery/packages-config.json")
-def config = new groovy.json.JsonSlurper().parseText(configFile)
+//def configFile = readFileFromWorkspace("./operations/continous-delivery/packages-config.json")
+//def config = new groovy.json.JsonSlurper().parseText(configFile)
 def artifactBucket = "glomex-infra-reposerver-prod"
 
 folder("packages") {
