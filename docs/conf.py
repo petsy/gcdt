a67e4822845d1ca30d436a44930040e70e1832f0
Jenkins Continuous Integration Server <infrajenkins@i-a13fa82a.instanceid.glomex.cloud>
2017-03-29 12:29:28 +0200

    Bump version: 0.0.83.dev1 → 0.0.83

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ author = u'glomex DevOps team & friends'
 # built documents.
 #
 # The short X.Y version.
-version = '0.0.83.dev1'
+version = '0.0.83'
 # The full version, including alpha/beta/rc tags.
 #release = '0.0.74'
 release = version
